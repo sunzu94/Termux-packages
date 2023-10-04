ba59c3a0468065cd5d9fff63c3dca82d972da2d7
Chongyun Lee <uchkks@protonmail.com>
2023-10-04 14:02:03 +0800

    libc++: do not extract source when cross-compile

--- packages/libc++/build.sh
+++ packages/libc++/build.sh
@@ -10,7 +10,24 @@ TERMUX_PKG_SHA256=1505c2297a5b7a04ed20b5d44da5665e91bac2b7c0fbcd3ae99b6ccc3a6128
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
+termux_step_get_source() {
+	if [ "$TERMUX_ON_DEVICE_BUILD" = true ]; then
+		termux_download_src_archive
+		cd $TERMUX_PKG_TMPDIR
+		termux_extract_src_archive
+	fi
+	mkdir -p $TERMUX_PKG_SRCDIR
+}
+
 termux_step_post_make_install() {
+	local _ndk_prefix
+
+	if [ "$TERMUX_ON_DEVICE_BUILD" = true ]; then
+		_ndk_prefix="$TERMUX_PKG_SRCDIR"
+	else
+		_ndk_prefix="$NDK"
+	fi
+
 	install -m700 -t "$TERMUX_PREFIX"/lib \
-		toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"${TERMUX_HOST_PLATFORM}"/libc++_shared.so
+		$_ndk_prefix/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/"${TERMUX_HOST_PLATFORM}"/libc++_shared.so
 }
